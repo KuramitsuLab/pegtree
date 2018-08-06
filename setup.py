4dbd16a09200181246616ac248c8c77ac92e184c
KASE <kaseyutaka.suffix@gmail.com>
2018-08-06 17:56:23 +0900

    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name = 'pegpy',
+    version = '0.1.0',
+    url = 'https://github.com/KuramitsuLab/pegpy.git',
+    license = 'KuramitsuLab',
+    author = 'KuramitsuLab',
+    description = 'Nez Parser for Python',
+    install_requires = ['setuptools'],
+	packages = ["pegpy"]
+)
