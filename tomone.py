0e2d6c218f51679aa3fc03a9e105f5275bae1619
Kimio Kuramitsu <kkuramitsu@gmail.com>
2018-11-06 14:17:37 +0900

    added comment tomone.py

--- tomone.py
+++ tomone.py
@@ -3,6 +3,16 @@ from pegpy.gpeg.gdasm import gdasm
 
 g = Grammar('tm')
 g.load('grammar/sample.gpeg')
+
+# sample.gpeg の example がテストされる
 g.testAll(gdasm)
 
+# parser が作られる
+parser = gdasm(g)
+
+# 文字列から ParseTree が得られる
+t = parser('aa')
+
+# ParseTree の表示
+print(t)
 
