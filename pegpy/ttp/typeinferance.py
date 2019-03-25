77f7b62e66608240ab2d8584be4c5300830005c3
tadataku-lab <tadataku.lab@gmail.com>
2019-03-25 16:31:05 +0900

    fet: typeinference ver2

--- pegpy/ttp/typeinferance.py
+++ pegpy/ttp/typeinferance.py
@@ -1,4 +1,4 @@
-from pegpy.ttp.tree_types import String, KeyValue, Tuple, List, Union, Option, Variable, Error, Rec
+from pegpy.ttp.tree_types import String, KeyValue, Tuple, List, Union, Option, Variable, Error
 
 
 class Env(object):
@@ -11,57 +11,125 @@ class Env(object):
 
 
 def TEmpty():
-  return lambda: String()
+  return lambda _: String()
 
 
-def TNt(nonterm, pf):
+def TChar():
+  return lambda _: String()
+
+
+def TValue():
+  return lambda _: String()
+
+
+def TNtFunc(nonterm, tf):
   def curry(env):
-    if not (nonterm in env.E):
-      env.E[nonterm] = Rec()
-      tau = pf(env)
-      if isinstance(tau, Rec):
-        # TODO: env.E[nonterm] =
-        env.E[nonterm] = pf(env)
-      else:
-        env.E[nonterm] = tau
-    elif isinstance(env.E[nonterm], Rec):
-
-    return env.E[nonterm]
+    if not (nonterm in env.Gamma):
+      env.Gamma[nonterm] = Variable(nonterm)
+      env.E[nonterm] = tf(env)
+    return env.Gamma[nonterm]
   return curry
 
 
-def TSeq(pf1, pf2):
+def TNt(pe, emit):
+  return TNtFunc(pe.uname(), emit(pe.deref()))
+
+
+def TSeqFunc(tf1, tf2):
   def curry(env):
-    tau1, tau2 = pf1(env), pf2(env)
-    if isinstance(tau1, String) or isinstance(tau2, List):
+    tau1, tau2 = tf1(env), tf2(env)
+    if isinstance(tau1, String):
       return tau2
-    if isinstance(tau2, String) or isinstance(tau1, List):
+    if isinstance(tau2, String):
+      return tau1
+    if isinstance(tau1, List) and isinstance(tau1.inner, type(tau2)):
+      return tau1
+    if isinstance(tau2, List) and isinstance(tau2.inner, type(tau1)):
+      return tau2
+    if isinstance(tau1, List) and isinstance(tau2, List) and isinstance(tau2.inner, type(tau1.inner)):
       return tau1
     if isinstance(tau1, KeyValue) and isinstance(tau2, KeyValue):
       # TODO: L1 != L2
       tau1.inner.update(tau2.inner)
       return tau1
-    if isinstance(tau1, Node) and isinstance(tau2, Node):
+    if isinstance(tau1, Variable) and isinstance(tau2, Variable):
       return Tuple([tau1, tau2])
     return Error(f'Inference Error ocurred in TSeq({tau1}, {tau2})')
   return curry
 
 
-def TAnd(pf):
-  return lambda env: pf(env)
+def TSeq(pe1, pe2, emit):
+  return TSeqFunc(emit(pe1), emit(pe2))
 
 
-def TNRep(pf):
-  return lambda env: List(pf(env))
-
-
-def TNode(nlabel, pf):
+def TOreFunc(tf1, tf2):
   def curry(env):
-    if not (nlabel in env.Gamma):
-      env.Gamma[nlabel] = pf(env)
-    return Variable(nlabel)
+    tau1, tau2 = tf1(env), tf2(env)
+    if isinstance(tau1, String) and isinstance(tau2, String):
+      return tau2
+    if isinstance(tau1, Variable) and isinstance(tau2, Variable):
+      return Union([tau1, tau2])
+    if isinstance(tau1, KeyValue) and isinstance(tau2, KeyValue):
+      # TODO: Option
+      tau1.inner.update(tau2.inner)
+      return tau1
+    return Error(f'Inference Error ocurred in TOre({tau1}, {tau2})')
   return curry
 
 
-def TEdge(elabel, pf):
-  return lambda env: KeyValue({elabel: pf(env)}) 
+def TOre(pe1, pe2, emit):
+  return TOreFunc(emit(pe1), emit(pe2))
+
+
+def TAndFunc(tf):
+  def curry(env):
+    return tf(env)
+  return curry
+
+
+def TAnd(pe, emit):
+  return TAndFunc(emit(pe))
+
+
+def TNot():
+  return lambda _: String()
+
+
+def TRepFunc(tf):
+  def curry(env):
+    tau = tf(env)
+    if isinstance(tau, String):
+      return tau
+    return List(tau)
+  return curry
+
+
+def TRep(pe, emit):
+  return TRepFunc(emit(pe))
+
+
+def TNodeFunc(nlabel, tf):
+  def curry(env):
+    if not (nlabel in env.Gamma):
+      env.Gamma[nlabel] = Variable(nlabel)
+      env.E[nlabel] = Variable(tf(env))
+    return env.Gamma[nlabel]
+  return curry
+
+
+def TNode(pe, emit):
+  return TNodeFunc(pe.label, emit(pe))
+
+
+def TEdgeFunc(elabel, tf):
+  def curry(env):
+    return KeyValue({elabel: tf(env)})
+  return curry
+
+
+def TEdge(pe, emit):
+  return TEdgeFunc(pe.label, emit(pe))
+
+
+def TAbs():
+  return lambda _: String()
