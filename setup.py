5088af872af3bbb0624c10b383ffe4c0b5eda55f
Kimio Kuramitsu <kkuramitsu@gmail.com>
2020-03-04 00:04:22 +0900

    0.9.5 checking left recursion

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ twine upload --repository pypi dist/*
 
 setup(
     name='pegtree',
-    version='0.9.4',
+    version='0.9.5',
     url='https://github.com/KuramitsuLab/pegtree.git',
     license='MIT',
     author='Kimio Kuramitsu',
