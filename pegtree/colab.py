ef820e5885f5dcfe9faa25480c0afcdc8fedbd95
Kimio Kuramitsu <kkuramitsu@gmail.com>
2020-10-07 09:03:55 +0900

    fix: @error in peg

--- pegtree/colab.py
+++ pegtree/colab.py
@@ -6,18 +6,20 @@ from IPython.core.magic import register_cell_magic
 @register_cell_magic
 def match(line, src):
   peg = pg.grammar(src)
-  parser = pg.generate(peg)
-  res = parser(line)
-  print(repr(res))
+  if '@error' not in peg:
+    parser = pg.generate(peg)
+    res = parser(line)
+    print(repr(res))
 
 
 @register_cell_magic
 def parse(line, src):
   peg = pg.grammar(src)
-  parser = pg.generate(peg)
-  res = parser(line)
-  if res.isSyntaxError():
-    print(repr(res))
-  else:
-    print(repr(res))
-    return graph.Viz(res) if res is not None else None
+  if '@error' not in peg:
+    parser = pg.generate(peg)
+    res = parser(line)
+    if res.isSyntaxError():
+      print(repr(res))
+    else:
+      print(repr(res))
+      return graph.Viz(res) if res is not None else None
