ee89c33f62fc5ce81cd980e0b048357bf2adef1f
Kimio <kkuramitsu@gmail.com>
2021-05-05 03:51:45 +0900

    add %%peg

--- pegtree/colab.py
+++ pegtree/colab.py
@@ -19,6 +19,7 @@ def parse_example(peg, line):
     if '@@example' in peg and len(peg['@@example']) > 0:
         name, doc = peg['@@example'][-1]
         parser = pg.generate(peg, start=str(name))
+        print(f'example: {name}', str(line))
         return parser(str(doc))
     else:
         parser = pg.generate(peg)
@@ -26,7 +27,7 @@ def parse_example(peg, line):
         return parser(line)
 
 @register_cell_magic
-def TPEG(line, src):
+def peg(line, src):
     peg = pg.grammar(src)
     if '@error' not in peg:
         tree = parse_example(peg, line)
@@ -36,20 +37,24 @@ def TPEG(line, src):
             print(repr(tree))
             return graph.draw_graph(tree) if tree is not None else None
 
-@register_cell_magic
-def pegtree(line, src):
-    return TPEG(line, src)
+def start_option(line):
+    if line.startswith('-s '):
+        _, start, path = line.split()
+        return start, path
+    return None, path
 
 @register_line_cell_magic
 def example(line, src=''):
-    print(line, line.strip())
-    peg = pg.grammar(line.strip())
+    start, path = start_option(line.strip())
+    peg = pg.grammar(path)
     if '@@example' not in peg:
         return
     parsers = {}
     for testcase in peg['@@example']:
         name, doc = testcase
         if not name in peg:
+            continue
+        if start is not None and name != start:
             continue
         if not name in parsers:
             parsers[name] = pg.generate(peg, start=name)
@@ -61,12 +66,23 @@ def example(line, src=''):
         display(Image(v.render()))
 
 @register_cell_magic
-def parse(line, src='None'):
-    peg = pg.grammar(line.strip())
-    parser = pg.generate(peg)
+def parse(line, src=''):
+    start, path = start_option(line.strip())
+    peg = pg.grammar(path)
+    if start is None:
+        parser = pg.generate(peg)
+    else:
+        parser = pg.generate(peg, start=start)
     tree = parser(src, urn='(stdin)')
     print(repr(tree))
     return graph.draw_graph(tree)
+
+@register_line_cell_magic
+def pegtree(line, src=''):
+    if line.endswith('.tpeg') or line.endswith('.pegtree'):
+        return parse(line, src)
+    else:
+        return peg(line, src)
 
 @register_line_cell_magic
 def match(line, src):
